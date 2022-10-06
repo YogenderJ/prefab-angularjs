78209da9f984fc8692118fd5b74f86b10fee44aa
Yogender Singh Jodha <yogender.jodha@magicedtech.com>
2022-10-06 17:51:30 +0530

    Codedeploy updated

--- Scripts/BeforeInstall.sh
+++ Scripts/BeforeInstall.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd /home/ubuntu/gitlab/angularjs
+cd /home/ubuntu/codepipeline/angularjs
 for pid in $(lsof -t -i:40545); do kill -9 $pid; done
 sudo rm -rf *
--- Scripts/Install.sh
+++ Scripts/Install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd /home/ubuntu/gitlab/angularjs
+cd /home/ubuntu/codepipeline/angularjs
 nohup ng serve --host 0.0.0.0 --port 40545 > /dev/null 2>&1 &
